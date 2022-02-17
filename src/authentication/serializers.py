25fc4497aec01530d75c33a880f0e6cf93448cee
Deias <saied_am@hotmail.com>
2022-02-17 18:20:06 +0100

    Rajout de la vue pour la création d'utilisateur, l'endpoint pour le login

---
+++ src/authentication/serializers.py
@@ -0,0 +1,21 @@
+from .models import User
+from rest_framework import serializers
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    """ Serializer for User model """
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        """ Create and return new user"""
+
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from authentication.serializers import CreateUserSerializer
+from rest_framework import generics
+from . import models
 
-# Create your views here.
+
+class CreateUserViewSet(generics.CreateAPIView):
+    """
+    Handels the creatinon of user
+    """
+
+    serializer_class = CreateUserSerializer
+    queryset = models.User.objects.all()
